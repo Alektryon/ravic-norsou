01a281dde1ed62180a55b7d586a87076ab65895e
Alektryon <84470810+Alektryon@users.noreply.github.com>
2022-08-31 16:48:04 +0100

    Add files via upload

--- calc/gematriaNGG.js
+++ calc/gematriaNGG.js
@@ -40,6 +40,14 @@ class cipher {
 				}
 				Build_GemVals(this)
 				break;
+			case "Kaye36":
+				for (y = 0; y < 36; y++) {
+				this.cArr = [48,49,50,51,52,53,54,55,56,57,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,38,8523,97,98,99,100,101,102,103,104,105,106]
+				this.cArr2 = [48,49,50,51,52,53,54,55,56,57,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,38,8523,65,66,67,68,69,70,71,72,73,74]
+				this.vArr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,35]
+				this.vArr2 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,35]
+                }
+				break;
 			case "AQ":
 				for (y = 0; y < 36; y++) {
 				this.cArr = [48,49,50,51,52,53,54,55,56,57,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122]
@@ -731,6 +739,7 @@ function Build_Ciphers() {
 			case "Bacon Short": allCiphers[allCiphers.length] = new cipher(key, "English", 100, 216, 209, "BaconShort"); break;
 			case "Bacon Short Reverse": allCiphers[allCiphers.length] = new cipher(key, "English", 88, 125, 254, "Reverse", "BaconShortRev"); break;
 			case "Bacon Kaye": allCiphers[allCiphers.length] = new cipher(key, "English", 220, 93, 73, "BaconKaye"); break;
+			case "Kaye 36": allCiphers[allCiphers.length] = new cipher(key, "Kaye36", 220, 93, 73); break;
 			case "Modern Kaye": allCiphers[allCiphers.length] = new cipher(key, "English", 230, 153, 163, "ModernKaye"); break;
 			case "Beatus of Liebana": allCiphers[allCiphers.length] = new cipher(key, "English", 192, 66, 255, "BeatusExtended"); break;
 			case "Beatus Ordinal": allCiphers[allCiphers.length] = new cipher(key, "English", 210, 87, 255, "BeatusOrdinal"); break;
@@ -822,6 +831,7 @@ function Set_Categories() {
 	cipherArray["Bacon Short"] = "English (Special)"
 	cipherArray["Bacon Short Reverse"] = "English (Special)"
 	cipherArray["Bacon Kaye"] = "English (Special)"
+	cipherArray["Kaye 36"] = "English (Special)"
 	cipherArray["Modern Kaye"] = "English (Special)"
 	cipherArray["Illuminati Novice"] = "English (Special)"
 	cipherArray["Illuminati Reverse"] = "English (Special)"
